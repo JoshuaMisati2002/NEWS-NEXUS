da47f0a96ad49154a2e44c74458a5f8730142a36
Joshua misati <misatijoshua6@gmail.com>
2025-08-07 15:53:59 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <div className="text-center p-8">
+        <p  className="text-4xl font-bold text-blue-500">
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p >
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
